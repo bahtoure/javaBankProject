06ec0ec807a9db0f7a36de26b52526eb860aeaf3
fatiha <fatihatoure@gmail.com>
2025-05-12 17:05:57 +0000

    modification du fichier

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
 class Main {
 	public static void main(String[] args){
 		
-		int[] entier = {10,20,30,40,50,60,70,80};
-		int[] entier2 = new int[8];
-		int nb =0;
-		for (int i=0; i<entier2.length; i++) {
-			nb +=2;
-			entier2[i]=nb;
+		System.out.println("hello github");
 		
-			System.out.println("indice["+i+"]"+entier2[i]);
-		}
 		
 		
 	
-		}
-		}
+	}
+}
